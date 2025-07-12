0ff1cb35191d1300bdcb63a52e3131031e000037
KevinSanchez <kevinsanchezcabrerakevin@gmail.com>
2025-07-11 20:40:21 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-using GestionAcademica.Data;
+﻿using GestionAcademica.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar DbContext con la cadena de conexi�n de la base de datos
+// Configuración de servicios
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -11,9 +11,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Habilitar CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirTodo",
@@ -22,14 +19,15 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod());
 });
 
-// Configurar el pipeline de solicitud HTTP
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var app = builder.Build();
+
+// ✅ Habilitar Swagger SIEMPRE (también en producción)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("PermitirTodo");
+
 app.MapControllers();
 app.Run();
+
